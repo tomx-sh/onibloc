8c12fb1405c9720a35d183fb668dac382c0727ef
Thomas DOUCHE <Thomas.douche@aol.com>
2024-10-20 13:09:02 +0200

    radix theme

--- app/(backend)/api/utils.ts
+++ app/(backend)/api/utils.ts
@@ -10,18 +10,29 @@ export async function callRpc(method: string, params: { [key: string]: string } 
     })
 
     const apiUrl = rpcUrl + "/" + method + (params[0] ? "?" + new URLSearchParams(params).toString() : "");
-    console.log(" RPC GET", apiUrl);
 
     try {
         const response = await fetch(apiUrl, { method: "GET", headers: requestHeaders});
 
         const data = await response.json();
 
-        if (data.error) return { error: data.error };
+        if (data.error) {
+            console.error(" RPC GET", apiUrl, data.error);
+            return { error: data.error };
+        }
+
+        console.log(" RPC GET", apiUrl, formattedKeys(data.result));
 
         return data.result;
 
     } catch (error) {
-        return { error: 'Thrown error: ' + (error as Error).message };
+        console.error(" RPC GET", apiUrl, error);
+        return { error: 'Thrown error: ' + (error as Error).message }
     }
+}
+
+
+/** Show the keys of an object like: { key1, key2, key3 } */
+function formattedKeys(object: any): string {
+    return `{ ${Object.keys(object).join(", ")} }`;
 }
--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,11 +1,16 @@
 'use client'
 import getBlockchainInfo from "../(backend)/api/get-blockchain-info/wrapper";
+import { Button } from "@radix-ui/themes";
+import { useState } from "react";
 
 
 export default function Home() {
+    const [isLoading, setIsLoading] = useState(false);
 
     async function handleClick() {
+        setIsLoading(true);
         const data = await getBlockchainInfo();
+        setIsLoading(false);
         console.log(data);
     }
 
@@ -15,7 +20,7 @@ export default function Home() {
     return (
         <main>
             <p>Hello</p>
-            <button onClick={handleClick}>Get blockchain info</button>
+            <Button onClick={handleClick} disabled={isLoading}>Get blockchain info</Button>
         </main>
     );
 }
